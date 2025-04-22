1e70472bc8de1183b2990404124ee8e5e7b54d50
marilio01 <mariliocavalcanti16@gmail.com>
2025-04-22 00:51:12 -0300

    feat: create orderProduct service

--- src/order/dtos/create-order.dto.ts
+++ src/order/dtos/create-order.dto.ts
@@ -1,15 +1,18 @@
 import { IsNumber, IsOptional, IsString } from 'class-validator';
- 
- export class CreateOrderDTO {
-   @IsOptional()
-   @IsNumber()
-   amountPayments?: number;
- 
-   @IsOptional()
-   @IsString()
-   codePix?: string;
- 
-   @IsOptional()
-   @IsString()
-   datePayment?: string;
- }
+
+export class CreateOrderDTO {
+  @IsNumber()
+  addressId: number;
+
+  @IsOptional()
+  @IsNumber()
+  amountPayments?: number;
+
+  @IsOptional()
+  @IsString()
+  codePix?: string;
+
+  @IsOptional()
+  @IsString()
+  datePayment?: string;
+}
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,20 +1,49 @@
 import { Injectable } from '@nestjs/common';
- import { InjectRepository } from '@nestjs/typeorm';
- import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { OrderEntity } from './entities/order.entity';
 import { CreateOrderDTO } from './dtos/create-order.dto';
 import { PaymentService } from '../payment/payment.service';
+import { CartService } from '../cart/cart.service';
+import { OrderProductService } from '../order-product/order-product.service';
+import { PaymentEntity } from '../payment/entities/payment.entity';
 
- @Injectable()
- export class OrderService {
-   constructor(
-     @InjectRepository(OrderEntity)
-     private readonly orderEntity: Repository<OrderEntity>,
-     private readonly paymentService: PaymentService,
-   ) {}
- 
-   async createOrder(createOrderDTO: CreateOrderDTO, cartId: number) {
-    await this.paymentService.createPayment(createOrderDTO);
-     return null;
-   }
- }
+@Injectable()
+export class OrderService {
+  constructor(
+    @InjectRepository(OrderEntity)
+    private readonly orderRepository: Repository<OrderEntity>,
+    private readonly paymentService: PaymentService,
+    private readonly cartService: CartService,
+    private readonly orderProductService: OrderProductService,
+  ) {}
+
+  async createOrder(
+    createOrderDTO: CreateOrderDTO,
+    cartId: number,
+    userId: number,
+  ) {
+    const payment: PaymentEntity =
+      await this.paymentService.createPayment(createOrderDTO);
+
+    const order = await this.orderRepository.save({
+      addressId: createOrderDTO.addressId,
+      date: new Date(),
+      paymentId: payment.id,
+      userId,
+    });
+
+    const cart = await this.cartService.findCartByUserId(userId, true);
+
+    cart.cartProduct?.forEach((cartProduct) => {
+      this.orderProductService.createOrderProduct(
+        cartProduct.productId,
+        order.id,
+        0,
+        cartProduct.amount,
+      );
+    });
+
+    return null;
+  }
+}
