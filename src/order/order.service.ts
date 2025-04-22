0df062c7a4a50209fc7c4d8f6218ac3c8218b43d
marilio01 <mariliocavalcanti16@gmail.com>
2025-04-21 22:54:05 -0300

    feat: create order controller

---
+++ src/order/order.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+ import { InjectRepository } from '@nestjs/typeorm';
+ import { Repository } from 'typeorm';
+import { OrderEntity } from './entities/order.entity';
+import { CreateOrderDTO } from './dtos/create-order.dto';
+
+ @Injectable()
+ export class OrderService {
+   constructor(
+     @InjectRepository(OrderEntity)
+     private readonly orderEntity: Repository<OrderEntity>,
+   ) {}
+ 
+   async createOrder(createOrderDTO: CreateOrderDTO, cartId: number) {
+     return null;
+   }
+ }
