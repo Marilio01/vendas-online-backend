ef8fe04a39a7da38582c764988a1f527ac20feee
marilio01 <mariliocavalcanti16@gmail.com>
2025-04-23 14:57:30 -0300

    feat: insert function getInfoUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -27,7 +27,7 @@ export class UserController {
   }
 
   @Roles(UserType.Admin)
-  @Get()
+  @Get('/all')
   async getAllUser(): Promise<ReturnUserDto[]> {
     return (await this.userService.getAllUser()).map(
       (UserEntity) => new ReturnUserDto(UserEntity),
@@ -48,5 +48,13 @@ export class UserController {
      @UserId() userId: number,
    ): Promise<UserEntity> {
      return this.userService.updatePasswordUser(updatePasswordDTO, userId);
+   }
+
+   @Roles(UserType.Admin, UserType.User)
+   @Get()
+   async getInfoUser(@UserId() userId: number): Promise<ReturnUserDto> {
+     return new ReturnUserDto(
+       await this.userService.getUserByIdUsingRelations(userId),
+     );
    }
 }
