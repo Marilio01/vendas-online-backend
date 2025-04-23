c047705828ee9b96c50a5d089ff9e89cb13c9d15
marilio01 <mariliocavalcanti16@gmail.com>
2025-04-23 08:55:06 -0300

    feat: create test unit in payment

--- src/payment/__tests__/payment.service.spec.ts
+++ src/payment/__tests__/payment.service.spec.ts
@@ -1,19 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentService } from '../payment.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+ import { Repository } from 'typeorm';
+ import { PaymentEntity } from '../entities/payment.entity';
 
 
 describe('PaymentService', () => {
   let service: PaymentService;
+  let paymentRepository: Repository<PaymentEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PaymentService],
+      providers: [
+        {
+          provide: getRepositoryToken(PaymentEntity),
+          useValue: {
+            save: jest.fn(),
+          },
+        },
+        PaymentService,
+      ],
     }).compile();
 
     service = module.get<PaymentService>(PaymentService);
+    paymentRepository = module.get<Repository<PaymentEntity>>(
+      getRepositoryToken(PaymentEntity),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(paymentRepository).toBeDefined();
   });
 });
