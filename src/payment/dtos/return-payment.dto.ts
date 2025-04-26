ab043fe93f3b71199b3ed632c28837a6442484f8
marilio01 <mariliocavalcanti16@gmail.com>
2025-04-25 23:56:09 -0300

    feat: insert returns and unit test in findOrderById

--- src/payment/dtos/return-payment.dto.ts
+++ src/payment/dtos/return-payment.dto.ts
@@ -1,22 +1,24 @@
-import { PaymentStatusEntity } from '../../payment-status/entities/payment-status.entity';
- import { PaymentEntity } from '../entities/payment.entity';
- 
- export class ReturnPaymentDTO {
-   id: number;
-   statusId: number;
-   price: number;
-   discount: number;
-   finalPrice: number;
-   type: string;
-   paymentStatus?: PaymentStatusEntity;
- 
-   constructor(payment: PaymentEntity) {
-     this.id = payment.id;
-     this.statusId = payment.statusId;
-     this.price = payment.price;
-     this.discount = payment.discount;
-     this.finalPrice = payment.finalPrice;
-     this.type = payment.type;
-     this.paymentStatus = payment.paymentStatus;
-   }
- }
+import { ReturnPaymentStatus } from '../../payment-status/dtos/return-payment-status.dto';
+import { PaymentEntity } from '../entities/payment.entity';
+
+export class ReturnPaymentDTO {
+  id: number;
+  statusId: number;
+  price: number;
+  discount: number;
+  finalPrice: number;
+  type: string;
+  paymentStatus?: ReturnPaymentStatus;
+
+  constructor(payment: PaymentEntity) {
+    this.id = payment.id;
+    this.statusId = payment.statusId;
+    this.price = payment.price;
+    this.discount = payment.discount;
+    this.finalPrice = payment.finalPrice;
+    this.type = payment.type;
+    this.paymentStatus = payment.paymentStatus
+      ? new ReturnPaymentStatus(payment.paymentStatus)
+      : undefined;
+  }
+}
